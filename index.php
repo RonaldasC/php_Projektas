ce76691d07ac003b60a960cedbf3035471c5fdd1
RonaldasC <ronaldasr@gmail.com>
2022-09-20 22:42:25 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<!--    <link rel="stylesheet" type="text/css" href="style.css"/>-->
+</head>
+<body>
+<header>
+    <H1><a href="SUPER ADWERTISMENT PORTAL OF ALL TIME AND SPACE"></a></H1>
+    <nav>
+        <a href="#">List ads</a>
+        <a href="#">Login</a>
+        <a href="#">Register</a>
+    </nav>
+    <main>
+        <aside>
+            <div>
+                <h3>VW Gold</h3>
+                <p>Price: 1 eur</p>
+                <p>Contact phone: 112</p>
+                <p>Created at: 2022-06-09 15:25:17</p>
+            </div>
+            <div>
+                <h3>VW Gold 6</h3>
+                <p>Price: 1500 eur</p>
+                <p>Contact phone: 911</p>
+                <p>Created at: 2022-06-09 17:15:14</p>
+            </div>
+            <div>
+                <h3>Car</h3>
+                <p>Price: 155 eur</p>
+                <p>Contact phone: 010203</p>
+                <p>Created at: 2022-06-09 15:27:11</p>
+            </div>
+            <div>
+                <h3>Car2</h3>
+                <p>Price: 123 eur</p>
+                <p>Contact phone: 291437</p>
+                <p>Created at: 2022-06-13 15:25:50</p>
+            </div>
+        </aside>
+    </main>
+</header>
+
+
+</body>
+</html>
